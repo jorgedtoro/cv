b3bb1399b2f283669892f4d46d0b0d96c22aaff1
Jorge de Toro <jorgedtoro@gmail.com>
2022-04-18 21:28:04 +0200

    update 18-04 MBP
    app js doesn't work...

--- app.js
+++ app.js
@@ -5,10 +5,28 @@ $(document).ready(function () {
     } else {
       $(".navbar").removeClass("sticky");
     }
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
   });
+
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+  });
+
   //   toggle menu/nvabar script
   $(".menu-btn").click(function () {
     $(".navbar .menu").toggleClass("active");
+  });
+  // typed animation script
+  var typed = new typed(".typing", {
+    strings: ["Full Stack Developer", "Blogger", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
   });
 
   //owl carousel script
